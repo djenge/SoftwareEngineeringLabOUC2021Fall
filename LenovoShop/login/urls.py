07efee26f2c052b62bb6fff8ca42f24e9450580d
yangMelody <70562502+yangMelody@users.noreply.github.com>
2021-12-13 01:10:13 +0800

    初始化了登录注册方法跟数据库的链接，初始化了获取商品评论

--- LenovoShop/login/urls.py
+++ LenovoShop/login/urls.py
@@ -4,4 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('login/', views.login, name='login'),
+    path('register/', views.register, name='register'),
+    path('reset_password/', views.reset_password, name='register'),
 ]
